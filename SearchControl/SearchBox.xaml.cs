96ce02ca8f63a9dc180cf221c91c19cf52ebdc73
Niels Schroyen <niels_schroyen@external.mckinsey.com>
2012-07-18 18:05:17 +0200

    added better search behavior

--- SearchControl/SearchBox.xaml.cs
+++ SearchControl/SearchBox.xaml.cs
@@ -1,6 +1,7 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
+using System.Windows.Input;
 using GitHistory.Settings;
 
 namespace GitHistory.SearchControl
@@ -16,6 +17,11 @@ namespace GitHistory.SearchControl
         }
 
         private void SearchClicked(object sender, RoutedEventArgs e)
+        {
+            Search();
+        }
+
+        private void Search()
         {
             var searchBoxViewModel = DataContext as SearchBoxViewModel;
             if (searchBoxViewModel != null)
@@ -33,6 +39,24 @@ namespace GitHistory.SearchControl
             }
            
 
+        }
+
+        private void ComboBoxSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Search();
+        }
+
+        private void ComboBoxKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                var searchBoxViewModel = DataContext as SearchBoxViewModel;
+                if (searchBoxViewModel != null)
+                {
+                    searchBoxViewModel.Comment = ((TextBox) sender).Text;
+                    searchBoxViewModel.DoSearch();
+                }
+            }
         }
     }
 }
